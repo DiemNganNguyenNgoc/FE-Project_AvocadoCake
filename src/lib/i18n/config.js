09dc4acacac615e80856596dbab7e145a0b07f15
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2025-08-20 11:05:31 +0700

    add-translation

--- src/lib/i18n/config.js
+++ src/lib/i18n/config.js
@@ -1,16 +1,40 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./locales/en/components.json";
-import vi from "./locales/vi/components.json";
+
+import enComponents from "../i18n/locales/en/components.json"
+import enPageProduct from "./locales/en/productPage.json"
+import viComponents from "../i18n/locales/vi/components.json";
+import viPageProduct from "./locales/vi/productPage.json"
+import viCommon from "./locales/vi/components.json"
+import enCommon from "./locales/en/components.json"
+
+
+
+
+const resources = {
+  vi: {
+    translation: {
+      ...viComponents,
+      ...viPageProduct,
+      ...viCommon,
+    }
+  },
+  en: {
+    translation: {
+      ...enComponents,
+      ...enPageProduct,
+      ...enCommon,
+
+    }
+  }
+};
 
 i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    vi: { translation: vi }
-  },
+  resources,
   lng: "vi",
   fallbackLng: "en",
-  interpolation: { escapeValue: false }
+  defaultNS: "translation"
 });
+
 
 export default i18n;
